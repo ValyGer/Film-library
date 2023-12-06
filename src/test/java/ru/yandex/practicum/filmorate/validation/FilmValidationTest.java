eeda78ee1e4052a78a15e016f2e60483a9f6fe1b
Valentin <valentin.geras@yandex.ru>
2023-12-06 13:23:16 +0300

    откоректировано оформление

--- src/test/java/ru/yandex/practicum/filmorate/validation/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validation/FilmValidationTest.java
@@ -11,12 +11,13 @@ class FilmValidationTest {
     FilmValidation filmValidation = new FilmValidation();
 
     @Test
-    void emptyNameIsPassed(){ // пустое название
+    void emptyNameIsPassed() { // пустое название
         Film film = new Film("", "adipisicing",
                 LocalDate.of(1965, 3, 25), 100L);
         Assertions.assertThrows(ValidationException.class, () ->
                 filmValidation.validateFilm(film), "Передано пустое название");
     }
+
     @Test
     void exceedingMaximumNumberOfCharactersInTheDescription() { // описание более 200 символов
         Film film = new Film("File name", "Пятеро друзей ( комик-группа «Шарло»), приезжают в город " +
@@ -26,13 +27,15 @@ class FilmValidationTest {
         Assertions.assertThrows(ValidationException.class, () ->
                 filmValidation.validateFilm(film), "Передано описание более 200 символов");
     }
+
     @Test
-    void yearOfReleaseIsLlessThantoday(){
+    void yearOfReleaseIsLlessThantoday() {
         Film film = new Film("File name", "adipisicing",
                 LocalDate.of(1765, 3, 25), 100L);
         Assertions.assertThrows(ValidationException.class, () ->
                 filmValidation.validateFilm(film), "Неверная дата релиза");
     }
+
     @Test
     void durationIsLessThanZero() {
         Film film = new Film("File name", "adipisicing",
@@ -40,8 +43,9 @@ class FilmValidationTest {
         Assertions.assertThrows(ValidationException.class, () ->
                 filmValidation.validateFilm(film), "Длительность отрицательная");
     }
+
     @Test
-    void EmptyRequest(){  // пустой запрос
+    void EmptyRequest() {  // пустой запрос
         Film film = new Film();
         Assertions.assertThrows(ValidationException.class, () ->
                 filmValidation.validateFilm(film), "Передается пустой запрос");
--- src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
@@ -14,37 +14,42 @@ class UserValidationTest {
     UserValidation userValidation = new UserValidation();
 
     @Test
-    void invalidEmailMissingCharacterAt(){ // в мэйле отсутствует @
+    void invalidEmailMissingCharacterAt() { // в мэйле отсутствует @
         User user = new User("mail.ru", "Login", "Nike Name", LocalDate.of(1986, 10, 25));
         Assertions.assertThrows(ValidationException.class, () ->
                         userValidation.validateUser(user), "Email не содержат символ @");
     }
+
     @Test
-    void blankEmail(){ // незаполнен мэйл
+    void blankEmail() { // незаполнен мэйл
         User user = new User("", "Login", "Nike Name", LocalDate.of(1986, 10, 25));
         Assertions.assertThrows(ValidationException.class, () ->
             userValidation.validateUser(user), "Email пустой");
     }
+
     @Test
-    void invalidLoginWithSpace(){ //передан логин с пробелом
+    void invalidLoginWithSpace() { //передан логин с пробелом
         User user = new User("mail@mail.ru", "Log in", "Nike Name", LocalDate.of(1986, 10, 25));
         Assertions.assertThrows(ValidationException.class, () ->
                 userValidation.validateUser(user), "Логин содержит пробел");
     }
+
     @Test
-    void replacingTheNameWithLogin(){ //замена имени логином
+    void replacingTheNameWithLogin() { //замена имени логином
         User user = new User("mail@mail.ru", "Login", "", LocalDate.of(1986, 10, 25));
         userController.createUser(user);
         Assertions.assertEquals("Login", user.getName(),"Имя должно быть заменено логином");
     }
+
     @Test
-    void incorrectDateOfBirth(){ //некорректная дата рождения
+    void incorrectDateOfBirth() { //некорректная дата рождения
         User user = new User("mail@mail.ru", "Login", "Nike Name", LocalDate.of(2024, 10, 25));
         Assertions.assertThrows(ValidationException.class, () ->
                 userValidation.validateUser(user), "Некорректная дата рождения");
     }
+
     @Test
-    void EmptyRequest(){  // пустой запрос
+    void EmptyRequest() {  // пустой запрос
         User user = new User();
         Assertions.assertThrows(ValidationException.class, () ->
             userValidation.validateUser(user), "Передается пустой запрос");
