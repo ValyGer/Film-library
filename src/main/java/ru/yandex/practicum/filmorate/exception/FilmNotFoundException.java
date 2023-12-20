a575b307f1b48bd1e4321fdf868245f178939f6e
Valentin <valentin.geras@yandex.ru>
2023-12-20 13:23:03 +0300

    Отработка checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -1,8 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class FilmNotFoundException extends RuntimeException {
-    public FilmNotFoundException(String message) {
-        super(message);
+    public FilmNotFoundException(String s) {
+        super(s);
     }
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/exception/UserNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
+    public UserNotFoundException(String s) {
+        super(s);
     }
 }
